c7922bf3147320902af2223069e2cf71d0e048a9
oppositecategory <christyaccount@hotmail.com>
2023-10-18 17:03:52 +0300

    Updated DQN implementation.

--- dqn.py
+++ dqn.py
@@ -7,6 +7,11 @@ from torch.distributions.uniform import Uniform
 from torch.optim import Adam
 
 import numpy as np 
+from tqdm import tqdm
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.use('tkagg')
 
 env = gym.make("CarRacing-v2",
               #render_mode='human',
@@ -22,6 +27,7 @@ N_buffer= 1000
 minibatch_size=32
 gamma = 0.99
 lr = 1e-2
+num_epochs = 10
 
 def conv_block(in_f, out_f, kernel_size, stride):
     return nn.Sequential(
@@ -41,6 +47,7 @@ def rgb2gray(rgb):
     r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
     return gray
+
 
 class QNetwork(nn.Module):
     def __init__(self, 
@@ -67,10 +74,10 @@ class QNetwork(nn.Module):
         self.N_buffer = N_buffer
         self.memory_index = 0 # Works modulo N_buffer
 
-        self.m_state = torch.zeros(N_buffer,input_dim[0],input_dim[0])
-        self.m_action = torch.zeros(N_buffer)
-        self.m_reward = torch.zeros(N_buffer)
-        self.m_state1 = torch.zeros(N_buffer,input_dim[0],input_dim[0])
+        self.m_state = torch.zeros(N_buffer,input_dim[0],input_dim[0], dtype=torch.float32)
+        self.m_action = torch.zeros(N_buffer,dtype=torch.long)
+        self.m_reward = torch.zeros(N_buffer,dtype=torch.float32)
+        self.m_state1 = torch.zeros(N_buffer,input_dim[0],input_dim[0],dtype=torch.float32)
 
         in_channels = [input_dim[2]] + [f[0] for f in filters] # [4,16,32]
         kernels_size = [f[1] for f in filters] # [8,4]
@@ -105,50 +112,37 @@ class QNetwork(nn.Module):
         self.m_state[self.memory_index,:,:] = s[0]
         self.m_action[self.memory_index] = s[1]
         self.m_reward[self.memory_index] = s[2]
-        if s[3]: 
+        if s[3] != None: 
             self.m_state1[self.memory_index,:,:] = s[3]
         self.memory_index = (self.memory_index + 1) % self.N_buffer
 
     def sample_experiences(self):
-        samples = np.random.randint(0, self.N_buffer,size=minibatch_size)
-        s_samples = torch.index_select(self.m_state, samples, index=0)
-        s_actions = torch.index_select(self.m_action, samples,index=0)
-        s_rewards = torch.index_select(self.m_actions, samples,index=0)
-        s_samples1 = torch.index_select(self.m_state1, samples,index=0)
-        return s_samples,s_actions,s_rewards,s_samples1
-
+        indices = torch.randint(0, self.N_buffer,size=(minibatch_size,))
+        s_samples = self.m_state[indices,:,:]
+        s_actions = self.m_action[indices]
+        s_rewards = self.m_reward[indices]
+        s_samples1 = self.m_state1[indices,:,:]
+        return (s_samples,s_actions,s_rewards,s_samples1)
 
 network = QNetwork()
 optimizer = Adam(network.parameters(), lr=lr)
 
-def process_experience_data(history):
-    """
-        Given history experience in form of: (state, action, reward, next_state)
-        the function process the history into the format of X,y for the neural network
-        to use for training the Q-function,
-    """
+@torch.no_grad()
+def process_experience_data(batch):
+    batch_state, batch_action, batch_reward,batch_next_s = batch
+    mask_next_s = torch.count_nonzero(batch_next_s,dim=(-2,-1))
+    return_approx = network(batch_next_s[:,None,:,:]).argmax(dim=1)
+    X = batch_state 
+    y = batch_reward[0] + mask_next_s[...,0] * return_approx
+    return X, batch_action, y
     
-
-    with torch.no_grad():
-        output = network(X1)
-    
-    y1 = r1 + gamma*torch.max(output,dim=-1)
-    y2 = r2
-
-    X = torch.cat([X1,X2])
-    y = torch.cat([y1,y2])
-
-    actions1 = torch.tensor([e[1] for e in history if e[3] != None], dtype=torch.long)
-    actions2 = torch.tensor([e[1] for e in history if e[3]  == None], dtype=torch.long)
-    actions = torch.cat([actions1,actions2])
-    return X,y,actions
-
-
 def train_epoch():
     obs, info = env.reset()
 
-    # We use the first 1000 frames to construct enough memory to sample and just let it interact.
-    for frame in range(batch_size + N_buffer):
+    episode_rewards = []
+    rewards = []
+    avg_batch_loss = []
+    for frame in tqdm(range(batch_size)):
         phi_curr = torch.tensor(rgb2gray(obs)[None,:,:],dtype=torch.float32)
 
         if np.random.binomial(1,epsilon,1):
@@ -157,9 +151,12 @@ def train_epoch():
             action = network(phi_curr).argmax().item()
 
         next_obs, reward, terminated, truncated, info = env.step(action)
+        rewards.append(reward)
 
         if terminated or truncated:
             obs, info = env.reset()
+            episode_rewards.append(np.mean(rewards))
+            rewards = []
             phi_next = None
         else:
             phi_next = torch.tensor(rgb2gray(next_obs)[None,:,:],dtype=torch.float32)
@@ -167,21 +164,35 @@ def train_epoch():
         s = (phi_curr, action, reward, phi_next)
         network.store_experience(s)
 
-        if frame < N_buffer:
+        if frame < minibatch_size:
             continue
         
         batch = network.sample_experiences()
-        X,y,actions = process_experience_data(batch)
-        
-        # optimizer.zero_grad()
-        # output = network(X)
-        # output = torch.index_select(output,actions,dim=-1)
-        # batch_loss = (output - y) ** 2
-        
-        # batch_loss.backward()
-        # optimizer.step()
-        # print(f'Batch: {frame-1000}, loss: {np.mean(batch_loss)}, average reward: {np.mean(output)}')
-    # return batch_loss
+        X,actions,y = process_experience_data(batch)
+
+        optimizer.zero_grad()
+        output = network(X[:,None,:,:])
+        output = output[range(minibatch_size),actions.long()]
+        batch_loss = ((output - y) ** 2).mean()
+
+        avg_batch_loss.append(batch_loss.item())
+        batch_loss.backward()
+        optimizer.step()
+    avg_episode_reward = np.mean(episode_rewards)
+    return avg_batch_loss,avg_episode_reward
+
+plt.ion()
+# figure, ax = plt.subplots(figsize=(10, 8))
+plt.title("Average Q")
+plt.xlabel("Training Epochs")
+plt.ylabel("Average action value")
+
+plt.axis([0,num_epochs,0,10])
 
 
-train_epoch()
+for epoch in range(num_epochs):
+    avg_batch_loss, avg_episode_reward = train_epoch()
+    plt.plot(epoch,avg_episode_reward)
+    plt.pause(0.05)
+
+plt.show()
